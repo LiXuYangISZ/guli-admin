6abe74ffcf71132c76e667eca3a624aa46a7817e
Leo <2422737092@qq.com>
2024-07-28 13:04:53 +0800

    统计分析

--- src/api/statistics.js
+++ src/api/statistics.js
@@ -1,11 +1,18 @@
 import request from '@/utils/request'
 
 export default {
-    //生成统计数据
+    //1. 生成统计数据
     createStatistics(day) {
         return request({
             url: `/staservice/daily/registerCount/${day}`,
             method: 'post',
+        })
+    },
+    // 2. 图标显示统计信息
+    showData(searchObj){
+        return request({
+            url: `/staservice/daily/showData/${searchObj.type}/${searchObj.begin}/${searchObj.end}`,
+            method: 'get',
         })
     }
 }
